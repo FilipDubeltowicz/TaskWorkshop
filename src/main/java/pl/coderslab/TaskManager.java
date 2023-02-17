3a5874f7371264ae86d28c3fa810fd85a825b673
FilipDubeltowicz <f.dubeltowicz@gmail.com>
2023-02-17 10:21:51 +0100

    main method add

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,5 +1,12 @@
 package pl.coderslab;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class TaskManager {
 
+    public static void main (String[] args){
+        // 1. Wczytywanie i wyświetlanie danych z pliku
+        
+        // 2. Kolejne metody
+    }
 }
