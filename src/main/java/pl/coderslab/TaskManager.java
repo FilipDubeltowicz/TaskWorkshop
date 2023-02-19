1e4f1bc9f41733cb093d2cb1db47c59d192a211d
FilipDubeltowicz <f.dubeltowicz@gmail.com>
2023-02-19 22:13:25 +0100

    list

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -2,11 +2,60 @@ package pl.coderslab;
 
 import org.apache.commons.lang3.StringUtils;
 
-public class TaskManager {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-    public static void main (String[] args){
-        // 1. Wczytywanie i wyświetlanie danych z pliku
-        
-        // 2. Kolejne metody
+public class TaskManager {
+    public static void printOption() {
+        System.out.println("Please select an option:");
+        String[] optionToSelect = {"add", "remove", "list", "exit"};
+        for (String options : optionToSelect) {
+            System.out.println(options);
+        }
+    }
+
+    public static void listTask() {
+        System.out.println("list");
+        File file = new File("tasks.csv");
+
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage() + "Nie ma pliku");
+        }
+        while (scanner.hasNextLine()) {
+            System.out.println(scanner.nextLine());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input;
+        do {
+            printOption();
+            input = scanner.nextLine();
+            switch (input) {
+                case "add":
+//                addTask();
+                    break;
+                case "remove":
+//                removeTask();
+                    break;
+                case "list":
+                    listTask();
+                    break;
+                case "exit":
+                    break;
+                default:
+                    System.out.println("Please select a correct option.");
+            }
+        } while (!input.equals("exit"));
     }
 }
+
+
+
